2b82a752332c3801630373957ffa2e414edfe209
Daniel Draper <ukdrq@student.kit.edu>
2016-05-08 20:02:41 +0200

    general formatting, naming of recursive functions updated.

--- opcua_impl_python_opcua/scripts/ros_client.py
+++ opcua_impl_python_opcua/scripts/ros_client.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python
 
-# Thanks to: https://github.com/ros-visualization/rqt_common_plugins/blob/groovy-devel/rqt_topic/src/rqt_topic/topic_widget.py
+# Thanks to:
+#  https://github.com/ros-visualization/rqt_common_plugins/blob/groovy-devel/rqt_topic/src/rqt_topic/topic_widget.py
 
-import rospy
-import roslib
-import roslib.message
-
-import datetime
-import time
 import sys
-
-import opcua
+import time
 
 from opcua import ua, Client
 
 
-class SubHandler(object):
-    def data_change(self, handle, node, val, attr):
-        print("Python: New data change event", handle, node, val, attr)
+def data_change(handle, node, val, attr):
+    print("Python: New data change event", handle, node, val, attr)
 
-    def event(self, handle, event):
-        print("Python: New event", handle, event)
+
+def event(handle, eventhandled):
+    print("Python: New event", handle, eventhandled)
+
+
+class SubHandler(object):
+    pass
 
 
 def main(args):
     client = Client("opc.tcp://localhost:21554")
     client.connect()
     root = client.get_root_node()
-    print(root.get_children())
-    #getting a variable by path and setting its value attribute
+    # getting a variable by path and setting its value attribute
     myvar = client.get_node(ua.NodeId(2001, 2))
-    print(myvar)
     datavalue = ua.Variant('Bok', ua.VariantType.String)
-    #datavalue.SourceTimestamp = 0
-    #datavalue.ServerTimestamp = 0
+    # datavalue.SourceTimestamp = 0
+    # datavalue.ServerTimestamp = 0
     myvar.set_value(datavalue)
 
-   # mode = client.get_node("ns=2;s=3001.Mode")
-   # mode.set_value(32, ua.VariantType.Int32)
+    # mode = client.get_node("ns=2;s=3001.Mode")
+    # mode.set_value(32, ua.VariantType.Int32)
 
-    #subscribing to data change event to our variable
+    # subscribing to data change event to our variable
     handler = SubHandler()
     sub = client.create_subscription(500, handler)
     sub.subscribe_data_change(myvar)
--- opcua_impl_python_opcua/scripts/ros_server.py
+++ opcua_impl_python_opcua/scripts/ros_server.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
-# Thanks to: https://github.com/ros-visualization/rqt_common_plugins/blob/groovy-devel/rqt_topic/src/rqt_topic/topic_widget.py
+# Thanks to:
+# https://github.com/ros-visualization/rqt_common_plugins/blob/groovy-devel/rqt_topic/src/rqt_topic/topic_widget.py
+import sys
+from PyQt4 import Qt
 
-import rospy
 import roslib
 import roslib.message
+import rospy
+from opcua import ua, Server
 
-import datetime
-import time
-import sys
+global server
 
-from opcua import ua, uamethod, Server
 
-class OpcUaROSTopic():
+class OpcUaROSTopic:
+    global server
 
     def __init__(self, parent, idx, topic_name, topic_type):
 
@@ -33,88 +35,45 @@ class OpcUaROSTopic():
 
         self._subscriber = rospy.Subscriber(self.name, self.message_class, self.message_callback)
 
-    def _recursive_create_items(self, parent, idx, topic_name, type_name, message):
+    def _recursive_create_items(self, parent, idx, topic_name, type_name, message, top_level=False):
         topic_text = topic_name.split('/')[-1]
         if '[' in topic_text:
             topic_text = topic_text[topic_text.index('['):]
 
         # This here are 'complex data'
         if hasattr(message, '__slots__') and hasattr(message, '_slot_types'):
-	    new_node = parent.add_object(idx,  parent.get_browse_name()) 
-            new_node.add_property(idx, parent.get_browse_name(), type_name)
+            complex_type = True
+            new_node = parent.add_object(ua.NodeId(topic_name, parent.nodeid.NamespaceIndex, ua.NodeIdType.String),
+                                         ua.QualifiedName(topic_text, parent.nodeid.NamespaceIndex))
+            new_node.add_property(ua.NodeId(topic_name + ".Type", idx),
+                                  ua.QualifiedName("Type", parent.nodeid.NamespaceIndex), type_name)
+            if top_level:
+                new_node.add_method(ua.NodeId(topic_name + ".Update", parent.nodeid.NamespaceIndex),
+                                    ua.QualifiedName("Update", parent.nodeid.NamespaceIndex),
+                                    self.opcua_update_callback, [], [])
             for slot_name, type_name in zip(message.__slots__, message._slot_types):
-                self._recursive_create_items(new_node, idx, topic_name + '/' + slot_name, type_name, getattr(message, slot_name))
+                self._recursive_create_items(new_node, idx, topic_name + '/' + slot_name, type_name,
+                                             getattr(message, slot_name))
 
         else:
             # This are arrays
-            base_type_str, array_size = self._extract_array_info(type_name)
+            base_type_str, array_size = _extract_array_info(type_name)
             try:
                 base_instance = roslib.message.get_message_class(base_type_str)()
             except (ValueError, TypeError):
                 base_instance = None
 
             if array_size is not None and hasattr(base_instance, '__slots__'):
+                complex_type = True
                 for index in range(array_size):
                     self._recursive_create_items(parent, idx, topic_name + '[%d]' % index, base_type_str, base_instance)
             else:
-                new_node = self._create_node_with_type(parent, idx, topic_name, topic_text, type_name, array_size)
+                new_node = _create_node_with_type(parent, idx, topic_name, topic_text, type_name, array_size)
 
+        # noinspection PyUnboundLocalVariable
         self._nodes[topic_name] = new_node
 
         return
-
-    def _extract_array_info(self, type_str):
-        array_size = None
-        if '[' in type_str and type_str[-1] == ']':
-            type_str, array_size_str = type_str.split('[', 1)
-            array_size_str = array_size_str[:-1]
-            if len(array_size_str) > 0:
-                array_size = int(array_size_str)
-            else:
-                array_size = 0
-
-        return type_str, array_size
-
-    def _create_node_with_type(self, parent, idx, topic_name, topic_text, type_name, array_size):
-
-        node = None
-        dv = None
-
-        if '[' in type_name:
-            type_name = type_name[:type_name.index('[')]
-
-        if type_name == 'bool':
-            dv = ua.Variant(False, ua.VariantType.Boolean)
-        elif type_name == 'byte':
-            dv = ua.Variant(0, ua.VariantType.Byte)
-        elif type_name == 'int8':
-            dv = ua.Variant(0, ua.VariantType.SByte)
-        elif type_name == 'uint8':
-            dv = ua.Variant(0, ua.VariantType.Byte)
-        elif type_name == 'int16':
-            dv = ua.Variant(0, ua.VariantType.Int16)
-        elif type_name == 'uint16':
-            dv = ua.Variant(0, ua.VariantType.UInt16)
-        elif type_name == 'int32':
-            dv = ua.Variant(0, ua.VariantType.Int32)
-        elif type_name == 'uint32':
-            dv = ua.Variant(0, ua.VariantType.UInt32)
-        elif type_name == 'int64':
-            dv = ua.Variant(0, ua.VariantType.Int64)
-        elif type_name == 'uint64':
-            dv = ua.Variant(0, ua.VariantType.UInt64)
-        elif type_name == 'float':
-            dv = ua.Variant(0.0, ua.VariantType.Float)
-        elif type_name == 'double':
-            dv = ua.Variant(0.0, ua.VariantType.Double)
-        elif type_name == 'string':
-            dv = ua.Variant('', ua.VariantType.String)
-        else:
-            print (type_name)
-            return None
-
-        if array_size is not None: value=[value for i in range(array_size)]
-        return parent.add_variable(idx, parent.get_browse_name(), dv.Value)
 
     def message_callback(self, message):
 
@@ -132,8 +91,10 @@ class OpcUaROSTopic():
                 if topic_name + '[%d]' % index in self._nodes:
                     self.update_value(topic_name + '[%d]' % index, slot)
                 else:
-                    base_type_str, _ = self._extract_array_info(self._nodes[topic_name].text(self._column_index['type']))
-                    self._recursive_create_items(self._nodes[topic_name], topic_name + '[%d]' % index, base_type_str, slot)
+                    base_type_str, _ = _extract_array_info(
+                        self._nodes[topic_name].text(self._column_index['type']))
+                    self._recursive_create_items(self._nodes[topic_name], topic_name + '[%d]' % index, base_type_str,
+                                                 slot, None)
             # remove obsolete children
             if len(message) < len(self._nodes[topic_name].get_children()):
                 for i in range(len(message), self._nodes[topic_name].childCount()):
@@ -144,17 +105,81 @@ class OpcUaROSTopic():
                 self._nodes[topic_name].set_value(repr(message))
 
     def _recursive_delete_widget_items(self, item):
-        def _recursive_remove_items_from_tree(item):
-            for index in reversed(range(item.childCount())):
-                _recursive_remove_items_from_tree(item.child(index))
-            topic_name = item.data(0, Qt.UserRole)
+        def _recursive_remove_items_from_tree(itemintree):
+            for index in reversed(range(itemintree.childCount())):
+                _recursive_remove_items_from_tree(itemintree.child(index))
+            topic_name = itemintree.data(0, Qt.UserRole)
             del self._tree_items[topic_name]
+
         _recursive_remove_items_from_tree(item)
         item.parent().removeChild(item)
 
 
-def main(args):
+def _extract_array_info(type_str):
+    array_size = None
+    if '[' in type_str and type_str[-1] == ']':
+        type_str, array_size_str = type_str.split('[', 1)
+        array_size_str = array_size_str[:-1]
+        if len(array_size_str) > 0:
+            array_size = int(array_size_str)
+        else:
+            array_size = 0
 
+    return type_str, array_size
+
+
+def _create_node_with_type(parent, idx, topic_name, topic_text, type_name, array_size):
+    node = None
+    dv = None
+
+    if '[' in type_name:
+        type_name = type_name[:type_name.index('[')]
+
+    if type_name == 'bool':
+        dv = ua.Variant(False, ua.VariantType.Boolean)
+    elif type_name == 'byte':
+        dv = ua.Variant(0, ua.VariantType.Byte)
+    elif type_name == 'int8':
+        dv = ua.Variant(0, ua.VariantType.SByte)
+    elif type_name == 'uint8':
+        dv = ua.Variant(0, ua.VariantType.Byte)
+    elif type_name == 'int16':
+        dv = ua.Variant(0, ua.VariantType.Int16)
+    elif type_name == 'uint16':
+        dv = ua.Variant(0, ua.VariantType.UInt16)
+    elif type_name == 'int32':
+        dv = ua.Variant(0, ua.VariantType.Int32)
+    elif type_name == 'uint32':
+        dv = ua.Variant(0, ua.VariantType.UInt32)
+    elif type_name == 'int64':
+        dv = ua.Variant(0, ua.VariantType.Int64)
+    elif type_name == 'uint64':
+        dv = ua.Variant(0, ua.VariantType.UInt64)
+    elif type_name == 'float':
+        dv = ua.Variant(0.0, ua.VariantType.Float)
+    elif type_name == 'double':
+        dv = ua.Variant(0.0, ua.VariantType.Double)
+    elif type_name == 'string':
+        dv = ua.Variant('', ua.VariantType.String)
+    else:
+        print (type_name)
+        return None
+
+    if array_size is not None:
+        value = []
+        for i in range(array_size):
+            value.append(i)
+    return parent.add_variable(ua.NodeId(topic_name, parent.nodeid.NamespaceIndex),
+                               ua.QualifiedName(topic_text, parent.nodeid.NamespaceIndex), dv.Value)
+
+
+def shutdown():
+    global server
+
+    server.stop()
+
+
+def main(args):
     rospy.init_node("opcua_server")
 
     server = Server()
@@ -170,14 +195,14 @@ def main(args):
 
         # get Objects node, this is where we should put our custom stuff
         objects = server.get_objects_node()
-	#Add a variable
-	myvar = objects.add_variable(idx, "myvar", 10)
-	myvar.set_writable()
-	
-        #TODO: Add refresh method (namespace can be sent as paramter)
+        # Add a variable
+        myvar = objects.add_variable(idx, "myvar", 10)
+        myvar.set_writable()
+
+        # TODO: Add refresh method (namespace can be sent as paramter)
 
         topics = objects.add_object(idx, "ROS-Topics")
-        #TODO: Add reading of system state
+        # TODO: Add reading of system state
 
         ros_topics = rospy.get_published_topics()
 
