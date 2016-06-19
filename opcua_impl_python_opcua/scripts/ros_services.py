9052aa6a2c20d24346225f9cbe2bc2110a467bf2
Daniel Draper <ukdrq@student.kit.edu>
2016-06-19 20:20:15 +0200

    input argument types for service method calls inserted. fixed refresh functions, changed class structure, removed global variables

--- opcua_impl_python_opcua/scripts/ros_services.py
+++ opcua_impl_python_opcua/scripts/ros_services.py
@@ -3,23 +3,19 @@
 # Thanks to:
 # https://github.com/ros-visualization/rqt_common_plugins/blob/groovy-devel/rqt_service_caller/src/rqt_service_caller/service_caller_widget.py
 import math
+import numpy
 import random
-import sys
 import time
 
 import genpy
 import rospy
 import rosservice
-from opcua import Server, ua
-
-global server
-global servicesDict
+from opcua import ua
 
 
 class OpcUaROSService:
-    global server
-
-    def __init__(self, parent, idx, service_name, service_class):
+    def __init__(self, server, parent, idx, service_name, service_class):
+        self.server = server
         self.name = service_name
         self._class = service_class
         self.proxy = rospy.ServiceProxy(self.name, self._class)
@@ -27,21 +23,31 @@ class OpcUaROSService:
         self._nodes = {}
         self.expressions = {}
         self._eval_locals = {}
-        self._eval_locals = {}
 
         for module in (math, random, time):
             self._eval_locals.update(module.__dict__)
         self._eval_locals['genpy'] = genpy
         del self._eval_locals['__name__']
         del self._eval_locals['__doc__']
-        parent.add_method(ua.NodeId(self.name, idx), self.name, self.call_service, [], [])
+        # Build the Array of inputs
+        inputs = []
+        sample_req = self._class._request_class()
+        for slot_name in sample_req.__slots__:
+            slot = getattr(sample_req, slot_name)
+            if hasattr(slot, '_type'):
+                slot_type = slot._type
+            else:
+                slot_type = primitivetovariant(type(slot))
+
+            inputs.append(slot_type)
+
+        parent.add_method(ua.NodeId(self.name, idx), self.name, self.call_service, inputs, [])
 
     def call_service(self, *args):
         print ("reached callback")
         request = self._class._request_class()
         print (request)
         self.fill_message_slots(request, self.name, self.expressions, self.counter)
-        print (request)
         try:
             response = self.proxy.call(request)
             return response
@@ -49,33 +55,41 @@ class OpcUaROSService:
             print(e)
 
     def fill_message_slots(self, message, topic_name, expressions, counter):
-        print("Filling message slots!")
-        if not hasattr(message, '__slots__'):
-            return
-        for slot_name in message.__slots__:
-            slot_key = topic_name + '/' + slot_name
+        try:
+            print("Filling message slots!")
 
-            # if no expression exists for this slot_key, continue with it's child slots
-            if slot_key not in expressions:
-                self.fill_message_slots(getattr(message, slot_name), slot_key, expressions, counter)
-                continue
-
-            expression = expressions[slot_key]
-            if len(expression) == 0:
-                continue
-
-            # get slot type
-            slot = getattr(message, slot_name)
-            if hasattr(slot, '_type'):
-                slot_type = slot._type
+            if not hasattr(message, '__slots__'):
+                print("message has no slots")
+                return
             else:
-                slot_type = type(slot)
+                print(message.__slots__)
+                for slot_name in message.__slots__:
+                    slot_key = topic_name + '/' + slot_name
+                    print ("filling slot " + slot_name)
+                    # if no expression exists for this slot_key, continue with it's child slots
+                    if slot_key not in expressions:
+                        self.fill_message_slots(getattr(message, slot_name), slot_key, expressions, counter)
+                        continue
 
-            self._eval_locals['i'] = counter
-            value = self._evaluate_expression(expression, slot_type)
-            if value is not None:
-                setattr(message, slot_name, value)
-            print(message)
+                    expression = expressions[slot_key]
+                    if len(expression) == 0:
+                        continue
+
+                    # get slot type
+                    slot = getattr(message, slot_name)
+                    if hasattr(slot, '_type'):
+                        slot_type = slot._type
+                    else:
+                        slot_type = primitivetovariant(type(slot))
+
+                    self._eval_locals['i'] = counter
+                    value = self._evaluate_expression(expression, slot_type)
+                    if value is not None:
+                        setattr(message, slot_name, value)
+                    print(message)
+                print (message)
+        except Exception as e:
+            print(e)
 
     def _evaluate_expression(self, expression, slot_type):
         successful_eval = True
@@ -104,55 +118,80 @@ class OpcUaROSService:
 
         return None
 
+    def recursive_delete_items(self, item):
+        for child in item.get_children():
+            self.recursive_delete_items(child)
+            if child in self._nodes:
+                del self._nodes[child]
+            self.server.delete_nodes([child])
+        self.server.delete_nodes([item])
 
-def refresh_services(idx, services_object_opc):
-    global servicesDict
+
+def primitivetovariant(typeofprimitive):
+    if typeofprimitive == bool:
+        dv = ua.VariantType.Boolean
+    elif typeofprimitive == numpy.byte:
+        dv = ua.VariantType.Byte
+    elif typeofprimitive == int:
+        dv = ua.VariantType.Int32
+    elif typeofprimitive == float:
+        dv = ua.VariantType.Float
+    elif typeofprimitive == numpy.double:
+        dv = ua.VariantType.Double
+    elif typeofprimitive == str:
+        dv = ua.VariantType.String
+    else:
+        print (typeofprimitive)
+        return None
+    return dv
+
+
+def refresh_services(server, servicesDict, idx, services_object_opc):
 
     rosservices = rosservice.get_service_list(include_nodes=False)
 
     for service_name_ros in rosservices:
         try:
             if service_name_ros not in servicesDict or servicesDict[service_name_ros] is None:
-                service = OpcUaROSService(services_object_opc, idx, service_name_ros,
+                service = OpcUaROSService(server, services_object_opc, idx, service_name_ros,
                                           rosservice.get_service_class_by_name(service_name_ros))
                 servicesDict[service_name_ros] = service
         except (rosservice.ROSServiceException, rosservice.ROSServiceIOException) as e:
             server.stop()
 
+    rosservices = rosservice.get_service_list(include_nodes=False)
     for service_nameOPC in servicesDict:
         found = False
         for rosservice_name in rosservices:
             if service_nameOPC == rosservice_name:
                 found = True
-        if not found:
+        if not found and servicesDict[service_nameOPC] is not None:
             servicesDict[service_nameOPC].recursive_delete_items(server.get_node(ua.NodeId(service_nameOPC, idx)))
-            del servicesDict[service_nameOPC]
+            servicesDict[service_nameOPC] = None
 
-
-def main(args):
-    global server
-    global servicesDict
-
-    rospy.init_node("opcua_server")
-    servicesDict = {}
-    server = Server()
-    server.set_endpoint("opc.tcp://0.0.0.0:21554/")
-    server.set_server_name("ROS ua Server")
-
-    server.start()  # setup our own namespace, this is expected
-    uri = "http://ros.org"
-    idx = server.register_namespace(uri)
-
-    # get Objects node, this is where we should put our custom stuff
-    objects = server.get_objects_node()
-
-    servicesopc = objects.add_object(idx, "ROS-Services")
-
-    while True:
-        refresh_services(idx, servicesopc)
-        time.sleep(2)
-    rospy.spin()
-
-
-if __name__ == "__main__":
-    main(sys.argv)
+# def main(args):
+#     global server
+#
+#     rospy.init_node("opcua_server")
+#     servicesDict = {}
+#     server = Server()
+#     server.set_endpoint("opc.tcp://0.0.0.0:21554/")
+#     server.set_server_name("ROS ua Server")
+#
+#     server.start()  # setup our own namespace, this is expected
+#     uri = "http://ros.org"
+#     idx = server.register_namespace(uri)
+#
+#     # get Objects node, this is where we should put our custom stuff
+#     objects = server.get_objects_node()
+#
+#     servicesopc = objects.add_object(idx, "ROS-Services")
+#
+#     while True:
+#         refresh_services(servicesDict, idx, servicesopc)
+#         time.sleep(2)
+#     rospy.spin()
+#
+#
+# if __name__ == "__main__":
+#     main(sys.argv)
